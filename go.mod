473790925dcd01325df20f37249be3fb4ab543bf
Steve Sklar <steven@questdb.io>
2023-04-20 16:36:03 -0400

    initial commit

---
+++ go.mod
@@ -0,0 +1,51 @@
+module github.com/sklarsa/prom-metrics-viewer
+
+go 1.20
+
+require (
+	github.com/prometheus/common v0.42.0
+	github.com/prometheus/prometheus v0.43.0
+	github.com/rivo/tview v0.0.0-20230406072732-e22ce9588bb4
+)
+
+require (
+	github.com/alecthomas/units v0.0.0-20211218093645-b94a6e3cc137 // indirect
+	github.com/aws/aws-sdk-go v1.44.217 // indirect
+	github.com/beorn7/perks v1.0.1 // indirect
+	github.com/cespare/xxhash/v2 v2.2.0 // indirect
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/dennwc/varint v1.0.0 // indirect
+	github.com/gdamore/encoding v1.0.0 // indirect
+	github.com/gdamore/tcell/v2 v2.6.0 // indirect
+	github.com/go-kit/log v0.2.1 // indirect
+	github.com/go-logfmt/logfmt v0.6.0 // indirect
+	github.com/gogo/protobuf v1.3.2 // indirect
+	github.com/golang/protobuf v1.5.3 // indirect
+	github.com/grafana/regexp v0.0.0-20221122212121-6b5c0a4cb7fd // indirect
+	github.com/jmespath/go-jmespath v0.4.0 // indirect
+	github.com/jpillora/backoff v1.0.0 // indirect
+	github.com/lucasb-eyer/go-colorful v1.2.0 // indirect
+	github.com/mattn/go-runewidth v0.0.14 // indirect
+	github.com/matttproud/golang_protobuf_extensions v1.0.4 // indirect
+	github.com/mwitkow/go-conntrack v0.0.0-20190716064945-2f068394615f // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/prometheus/client_golang v1.14.0 // indirect
+	github.com/prometheus/client_model v0.3.0 // indirect
+	github.com/prometheus/common/sigv4 v0.1.0 // indirect
+	github.com/prometheus/procfs v0.9.0 // indirect
+	github.com/rivo/uniseg v0.4.3 // indirect
+	github.com/stretchr/testify v1.8.2 // indirect
+	go.uber.org/atomic v1.10.0 // indirect
+	go.uber.org/goleak v1.2.1 // indirect
+	golang.org/x/exp v0.0.0-20230307190834-24139beb5833 // indirect
+	golang.org/x/net v0.8.0 // indirect
+	golang.org/x/oauth2 v0.6.0 // indirect
+	golang.org/x/sys v0.6.0 // indirect
+	golang.org/x/term v0.6.0 // indirect
+	golang.org/x/text v0.8.0 // indirect
+	google.golang.org/appengine v1.6.7 // indirect
+	google.golang.org/protobuf v1.29.0 // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+)
---
+++ main.go
@@ -0,0 +1,197 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"os"
+	"sort"
+	"strings"
+	"sync"
+	"time"
+
+	"github.com/prometheus/common/model"
+	"github.com/prometheus/prometheus/config"
+	"github.com/prometheus/prometheus/discovery/targetgroup"
+	"github.com/prometheus/prometheus/model/exemplar"
+	"github.com/prometheus/prometheus/model/histogram"
+	"github.com/prometheus/prometheus/model/labels"
+	"github.com/prometheus/prometheus/model/metadata"
+	"github.com/prometheus/prometheus/scrape"
+	"github.com/prometheus/prometheus/storage"
+	"github.com/rivo/tview"
+)
+
+type InMemoryMetricStorage struct {
+	appender storage.Appender
+}
+
+type RegularValue struct {
+	Labels    labels.Labels
+	Timestamp int64
+	Value     float64
+}
+
+func (v RegularValue) String() string {
+	var (
+		metricName string
+		labelPairs []string
+	)
+	for _, l := range v.Labels {
+		switch l.Name {
+		case labels.MetricName:
+			metricName = l.Value
+		case labels.InstanceName, "job":
+			continue
+		default:
+			labelPairs = append(labelPairs, fmt.Sprintf("%s=%s", l.Name, l.Value))
+		}
+	}
+
+	return fmt.Sprintf("%s{%s} %f", metricName, strings.Join(labelPairs, ","), v.Value)
+}
+
+type InMemoryAppender struct {
+	data map[string]RegularValue
+	mu   *sync.Mutex
+}
+
+func (s InMemoryMetricStorage) Appender(ctx context.Context) storage.Appender {
+	return s.appender
+}
+
+func (a *InMemoryAppender) Append(ref storage.SeriesRef, l labels.Labels, t int64, v float64) (storage.SeriesRef, error) {
+	a.mu.Lock()
+	defer a.mu.Unlock()
+	a.data[l.String()] = RegularValue{Labels: l, Timestamp: t, Value: v}
+	return ref, nil
+}
+func (a *InMemoryAppender) Commit() error {
+	return nil
+}
+func (a *InMemoryAppender) Rollback() error {
+	return nil
+}
+func (a *InMemoryAppender) AppendExemplar(ref storage.SeriesRef, l labels.Labels, e exemplar.Exemplar) (storage.SeriesRef, error) {
+	println("exemplar", l.String(), e.Ts, e.Value, e.Labels.String())
+	return ref, nil
+}
+func (a *InMemoryAppender) AppendHistogram(ref storage.SeriesRef, l labels.Labels, t int64, h *histogram.Histogram, fh *histogram.FloatHistogram) (storage.SeriesRef, error) {
+	println("histogram", l.String(), t, h, fh)
+	return ref, nil
+}
+func (a *InMemoryAppender) UpdateMetadata(ref storage.SeriesRef, l labels.Labels, m metadata.Metadata) (storage.SeriesRef, error) {
+	println("metadata", l.String(), m.Help, m.Type, m.Unit)
+	return ref, nil
+}
+
+func main() {
+	if len(os.Args) != 2 {
+		panic("Usage: metrics-viewer <metrics-host>")
+	}
+
+	host := os.Args[1]
+	scheme := "http"
+	scrapeTimeout := time.Millisecond * 500 // todo: make this configurable
+	scrapeInterval := time.Second * 1       // todo: make this configurable
+	refreshInterval := time.Second * 1      // todo: make this configurable
+
+	ticker := time.NewTicker(refreshInterval)
+
+	cfg := &config.Config{
+		ScrapeConfigs: []*config.ScrapeConfig{{
+			Scheme:         scheme,
+			MetricsPath:    "/metrics",
+			JobName:        "metrics-viewer",
+			ScrapeInterval: model.Duration(scrapeInterval),
+			ScrapeTimeout:  model.Duration(scrapeTimeout),
+		}},
+	}
+
+	storage := InMemoryMetricStorage{
+		appender: &InMemoryAppender{
+			data: make(map[string]RegularValue),
+			mu:   &sync.Mutex{},
+		},
+	}
+
+	mgr := scrape.NewManager(&scrape.Options{}, nil, storage)
+	err := mgr.ApplyConfig(cfg)
+	if err != nil {
+		panic(err)
+	}
+
+	ts := make(chan map[string][]*targetgroup.Group)
+	go mgr.Run(ts)
+
+	defer mgr.Stop()
+
+	res := labels.FromMap(map[string]string{
+		model.AddressLabel:        host,
+		model.InstanceLabel:       host,
+		model.SchemeLabel:         scheme,
+		model.MetricsPathLabel:    "/metrics",
+		model.JobLabel:            "metrics-viewer",
+		model.ScrapeIntervalLabel: scrapeInterval.String(),
+		model.ScrapeTimeoutLabel:  scrapeTimeout.String(),
+	})
+
+	ls := model.LabelSet{}
+	for _, l := range res {
+		ls[model.LabelName(l.Name)] = model.LabelValue(l.Value)
+	}
+
+	ts <- map[string][]*targetgroup.Group{
+		"metrics-viewer": {
+			{
+				Targets: []model.LabelSet{ls},
+			},
+		},
+	}
+
+	println("waiting to register target...")
+	for len(mgr.TargetsActive()) == 0 {
+		time.Sleep(250 * time.Millisecond)
+	}
+
+	tgt := mgr.TargetsAll()["metrics-viewer"][0]
+	fmt.Printf("%s %s %s", tgt.LastScrape(), tgt.LastScrapeDuration(), tgt.Health())
+
+	app := tview.NewApplication()
+	list := tview.NewList()
+	list.ShowSecondaryText(false)
+
+	go func() {
+		for range ticker.C {
+
+			keys := []string{}
+			data := storage.appender.(*InMemoryAppender).data
+
+			for k := range data {
+				keys = append(keys, k)
+			}
+			sort.Strings(keys)
+
+			for _, k := range keys {
+				// doesn't keep this alpha, just adds new ones to the bottom
+
+				found := list.FindItems("dummy value", k, false, true)
+				item := data[k].String()
+				switch len(found) {
+				case 0:
+					list.AddItem(item, k, 0, nil)
+				case 1:
+					list.SetItemText(found[0], item, k)
+				default:
+					// todo: handle this better
+					panic("found multiple items")
+
+				}
+			}
+
+			app.Draw()
+		}
+	}()
+
+	app.SetRoot(list, true).Run()
+
+}
